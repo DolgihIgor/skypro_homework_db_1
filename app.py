483e6c7e0542f1eb5505db6dd3b7ddee861c0424
DolgihIgor <igor_dolgih@inbox.ru>
2023-05-03 10:11:09 +0700

    Finally project

--- app.py
+++ app.py
@@ -126,7 +126,52 @@ def main():
             })
         return jsonify(response_json)
 
-    app.run()
+    def get_actors(name1, name2):
+        query = f"""
+        SELECT
+            "cast"
+        FROM netflix
+        WHERE "cast" LIKE '%{name1}%'
+            AND "cast" LIKE '%{name2}%'        
+        """
+
+        response = db_connect(query)
+        actors = []
+        for cast in response:
+            actors.extend(cast[0].split(', '))
+        result = []
+        for a in actors:
+            if a not in [name1, name2]:
+                if actors.count(a) > 2:
+                    result.append(a)
+        result = set(result)  # добавим во множество, чтобы исключить повторения
+
+        print(result)
+
+    def get_films(type_film, release_year, genre):
+        query = f"""
+            SELECT
+                title
+                , description
+            FROM netflix
+            WHERE "type" = '{type_film}'
+                AND release_year = {release_year}
+                AND listed_in LIKE '%{genre}%'
+        """
+
+        response = db_connect(query)
+        response_json = []
+        for film in response:
+            response_json.append({
+                'title': film[0],
+                'description': film[1]
+            })
+        for film in response_json:
+            print(film)
+
+    # get_films('Movie', '2016', 'Dramas')
+    # get_actors('Rose McIver', 'Ben Lamb')
+    # app.run()
 
 
 if __name__ == '__main__':
